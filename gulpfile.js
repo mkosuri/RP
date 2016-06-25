e1d3ae2e3f488f08bbccdc9faa7d1518b655bbac
mkosuri <mkosuri@navaratan.com>
2016-06-25 23:41:57 +0530

    new changes added to commited

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 var gulp = require('gulp'),
-    usemin = require('gulp-usemin'),
-    wrap = require('gulp-wrap'),
-    connect = require('gulp-connect'),
-    watch = require('gulp-watch'),
-    minifyCss = require('gulp-cssnano'),
-    minifyJs = require('gulp-uglify'),
-    concat = require('gulp-concat'),
-    less = require('gulp-less'),
-    rename = require('gulp-rename'),
-    minifyHTML = require('gulp-htmlmin');
+usemin = require('gulp-usemin'),
+wrap = require('gulp-wrap'),
+connect = require('gulp-connect'),
+watch = require('gulp-watch'),
+minifyCss = require('gulp-cssnano'),
+minifyJs = require('gulp-uglify'),
+concat = require('gulp-concat'),
+less = require('gulp-less'),
+rename = require('gulp-rename'),
+minifyHTML = require('gulp-htmlmin');
 
 var paths = {
     scripts: 'src/js/**/*.*',
@@ -22,61 +22,61 @@ var paths = {
 /**
  * Handle bower components from index
  */
-gulp.task('usemin', function() {
+ gulp.task('usemin', function() {
     return gulp.src(paths.index)
-        .pipe(usemin({
-            js: [minifyJs(), 'concat'],
-            css: [minifyCss({keepSpecialComments: 0}), 'concat'],
-        }))
-        .pipe(gulp.dest('dist/'));
+    .pipe(usemin({
+        js: [minifyJs(), 'concat'],
+        css: [minifyCss({keepSpecialComments: 0}), 'concat'],
+    }))
+    .pipe(gulp.dest('dist/'));
 });
 
 /**
  * Copy assets
  */
-gulp.task('build-assets', ['copy-bower_fonts']);
+ gulp.task('build-assets', ['copy-bower_fonts']);
 
-gulp.task('copy-bower_fonts', function() {
+ gulp.task('copy-bower_fonts', function() {
     return gulp.src(paths.bower_fonts)
-        .pipe(rename({
-            dirname: '/fonts'
-        }))
-        .pipe(gulp.dest('dist/lib'));
+    .pipe(rename({
+        dirname: '/fonts'
+    }))
+    .pipe(gulp.dest('dist/lib'));
 });
 
 /**
  * Handle custom files
  */
-gulp.task('build-custom', ['custom-images', 'custom-js', 'custom-less', 'custom-templates']);
+ gulp.task('build-custom', ['custom-images', 'custom-js', 'custom-less', 'custom-templates']);
 
-gulp.task('custom-images', function() {
+ gulp.task('custom-images', function() {
     return gulp.src(paths.images)
-        .pipe(gulp.dest('dist/img'));
+    .pipe(gulp.dest('dist/img'));
 });
 
-gulp.task('custom-js', function() {
+ gulp.task('custom-js', function() {
     return gulp.src(paths.scripts)
-        .pipe(minifyJs())
-        .pipe(concat('dashboard.min.js'))
-        .pipe(gulp.dest('dist/js'));
+    .pipe(minifyJs())
+    .pipe(concat('dashboard.min.js'))
+    .pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('custom-less', function() {
+ gulp.task('custom-less', function() {
     return gulp.src(paths.styles)
-        .pipe(less())
-        .pipe(gulp.dest('dist/css'));
+    .pipe(less())
+    .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('custom-templates', function() {
+ gulp.task('custom-templates', function() {
     return gulp.src(paths.templates)
-        .pipe(minifyHTML())
-        .pipe(gulp.dest('dist/templates'));
+    .pipe(minifyHTML())
+    .pipe(gulp.dest('dist/templates'));
 });
 
 /**
  * Watch custom files
  */
-gulp.task('watch', function() {
+ gulp.task('watch', function() {
     gulp.watch([paths.images], ['custom-images']);
     gulp.watch([paths.styles], ['custom-less']);
     gulp.watch([paths.scripts], ['custom-js']);
@@ -87,7 +87,7 @@ gulp.task('watch', function() {
 /**
  * Live reload server
  */
-gulp.task('webserver', function() {
+ gulp.task('webserver', function() {
     connect.server({
         root: 'dist',
         livereload: true,
@@ -95,14 +95,15 @@ gulp.task('webserver', function() {
     });
 });
 
-gulp.task('livereload', function() {
+ gulp.task('livereload', function() {
     gulp.src(['dist/**/*.*'])
-        .pipe(watch(['dist/**/*.*']))
-        .pipe(connect.reload());
+    .pipe(watch(['dist/**/*.*']))
+    .pipe(connect.reload());
 });
 
 /**
  * Gulp tasks
  */
-gulp.task('build', ['usemin', 'build-assets', 'build-custom']);
-gulp.task('default', ['build', 'webserver', 'livereload', 'watch']);
+ gulp.task('build', ['usemin', 'build-assets', 'build-custom']);
+ gulp.task('default', ['build', 'webserver', 'livereload', 'watch']);
+ 
--- src/js/routes.js
+++ src/js/routes.js
@@ -7,7 +7,7 @@ angular.module('RDash').config(['$stateProvider', '$urlRouterProvider',
   function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/login');
 
-      $stateProvider.state('index', {
+      $stateProvider.state('dashboard', {
         url: '/dashboard',
         views: {
           "main": {templateUrl: 'templates/dashboard.html'}
@@ -19,6 +19,29 @@ angular.module('RDash').config(['$stateProvider', '$urlRouterProvider',
           "main": {templateUrl: 'templates/login.html'}
         }
       })
-
+      $stateProvider.state('regions', {
+        url: '/regions',
+        views: {
+          "main": {templateUrl: 'templates/regions.html'}
+        }
+      })
+      $stateProvider.state('operator', {
+        url: '/operator',
+        views: {
+          "main": {templateUrl: 'templates/operator.html'}
+        }
+      })
+      $stateProvider.state('categorier', {
+        url: '/categorier',
+        views: {
+          "main": {templateUrl: 'templates/categoriers.html'}
+        }
+      })
+      $stateProvider.state('plans-offers', {
+        url: '/plans-offers',
+        views: {
+          "main": {templateUrl: 'templates/plans_offers.html'}
+        }
+      })
     }
 ]);
